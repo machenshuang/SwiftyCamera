f94bc3081970977cc2094972f484af662f662a88
machenshuang <machenshuang@joyy.com>
2023-04-07 17:56:21 +0800

    feat：生成pod库

---
+++ Example/SwiftyCamera/SYViewController.h
@@ -0,0 +1,13 @@
+//
+//  SYViewController.h
+//  SwiftyCamera
+//
+//  Created by chenshuangma@foxmail.com on 04/07/2023.
+//  Copyright (c) 2023 chenshuangma@foxmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SYViewController : UIViewController
+
+@end
---
+++ Example/SwiftyCamera/SYViewController.m
@@ -0,0 +1,30 @@
+//
+//  SYViewController.m
+//  SwiftyCamera
+//
+//  Created by chenshuangma@foxmail.com on 04/07/2023.
+//  Copyright (c) 2023 chenshuangma@foxmail.com. All rights reserved.
+//
+
+#import "SYViewController.h"
+#import "SYCamera.h"
+
+@interface SYViewController ()
+
+@end
+
+@implementation SYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
