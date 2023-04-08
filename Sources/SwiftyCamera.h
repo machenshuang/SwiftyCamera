709a880c34ae041c37dfed9838c291dd2b34b9cd
SeanMa <chenshuangma@foxmail.com>
2023-04-08 17:46:55 +0800

    feat：调整 podspec

---
+++ Sources/SwiftyCamera.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyCamera.h
+//  SwiftyCamera
+//
+//  Created by 马陈爽 on 2023/4/8.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyCamera.
+FOUNDATION_EXPORT double SwiftyCameraVersionNumber;
+
+//! Project version string for SwiftyCamera.
+FOUNDATION_EXPORT const unsigned char SwiftyCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyCamera/PublicHeader.h>
+
+
+#import "SYCamera.h"
--- SwiftyCamera.podspec
+++ SwiftyCamera.podspec
@@ -29,8 +29,8 @@ TODO: Add long description of the pod here.
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '11.0'
-
-  s.source_files = 'Source/**/*'
-  s.public_header_files = "Source/*.h"
+  s.static_framework = true
+  s.source_files = 'Sources/**/*'
+  s.public_header_files = "Sources/**/*.h"
 
 end
